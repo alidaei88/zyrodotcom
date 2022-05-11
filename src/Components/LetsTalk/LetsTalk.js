6b965c60bc6b63b778d86808b38c983ae7bcb28b
ali.daeiلهف <ali.daei.dbm@gmail.com>
2022-05-11 23:38:34 +0430

    add validation to useform

--- src/Components/LetsTalk/LetsTalk.js
+++ src/Components/LetsTalk/LetsTalk.js
@@ -1,46 +1,47 @@
 import './LetsTalk.css';
 import contactMeImg from '../../public/contactme.avif'
 import { useState } from 'react';
+import { useForm } from '../../modules/useForm';
 
-function useForm(initailData) {
-    const [data, setData] = useState(initailData)
-    const [isEmailValid, setIsEmailValid] = useState(true);
-    const [isPassValid, setIsPassValid] = useState(true);
+// function useForm(initailData) {
+//     const [data, setData] = useState(initailData)
+//     const [isEmailValid, setIsEmailValid] = useState(true);
+//     const [isPassValid, setIsPassValid] = useState(true);
 
-    const changeHandler = (key) => (e) => {
-        const value = e.target.value;
+//     const changeHandler = (key) => (e) => {
+//         const value = e.target.value;
 
-        setData({
-            ...data,
-            [key]: value,
-        });
+//         setData({
+//             ...data,
+//             [key]: value,
+//         });
 
         
-    }    
+//     }    
 
-    // click handler is remove inputs data
-    const clickHandler = () => {
-        setData(initailData)
-    }
+//     // click handler is remove inputs data
+//     const clickHandler = () => {
+//         setData(initailData)
+//     }
 
-    // check for form validation
-    const isValid = () =>  {
+//     // check for form validation
+//     const isValid = () =>  {
         
-        const insertedEmail = data?.email;
-        const insertedPassword = data?.password;
+//         const insertedEmail = data?.email;
+//         const insertedPassword = data?.password;
         
-        if (!insertedEmail?.includes(".com") && !insertedEmail?.includes("@")) {
-             setIsEmailValid(false)
-        } else setIsEmailValid(true);
+//         if (!insertedEmail?.includes(".com") && !insertedEmail?.includes("@")) {
+//              setIsEmailValid(false)
+//         } else setIsEmailValid(true);
 
-        if (insertedPassword.length <= 8) {
-            setIsPassValid(false)
-        } else setIsPassValid(true)
+//         if (insertedPassword.length <= 8) {
+//             setIsPassValid(false)
+//         } else setIsPassValid(true)
  
-    }
+//     }
 
-    return [ data, isEmailValid, isPassValid, changeHandler, isValid]
-}
+//     return [ data, isEmailValid, isPassValid, changeHandler, isValid]
+// }
 
 
 
